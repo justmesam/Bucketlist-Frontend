ec39b84f15353551f0e8df4eace61cded70fd50e
justmesam <samuel.wanjiru@andela.com>
2017-09-21 11:54:59 +0300

    [FEATURE] Handle how data will be rendered after the user logs in

---
+++ src/containers/dashboard.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import FlatButton from 'material-ui/FlatButton';
+import NavBar from '../components/layout/navBars'
+import BucketContainer from './bucketlist';
+
+class Dashboard extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+    <div>
+     <div>
+       <NavBar style={{margin:15}}/>
+       <BucketContainer />
+     </div>
+    </div>
+);
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { bucketlists } = state.bucketlists;
+  return {
+    bucketlists
+  }
+}
+export default connect(mapStateToProps)(Dashboard);
+export { Dashboard }
