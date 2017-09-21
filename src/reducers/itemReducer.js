62a7ba531c0af2104266edd3eb0cb26774af3ac8
justmesam <samuel.wanjiru@andela.com>
2017-09-21 10:57:50 +0300

    [FEATURE] Add the item reducer that manages the store on the state of the actions after the actions are fulfilled

---
+++ src/reducers/itemReducer.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+
+const initial_state = {
+  allItems: [],
+  singleItem : null,
+};
+
+export default (state=initial_state, action) => {
+  switch (action.type) {
+    case "GET_BUCKETLIST_ITEMS_FULFILLED":
+      return{
+        ...state,
+        allItems: action.payload.data
+      }
+    case "GET_ONE_BUCKETLIST_ITEM_FULFILLED":
+      return {
+        ...state,
+        singleItem: action.payload.data
+      }
+    case "POST_BUCKETLIST_ITEMS_FULFILLED":
+      return {
+        ...state,
+        allItems: _.concat(state.allItems, action.payload.data)
+      }
+    case "EDIT_BUCKETLIST_ITEMS_FULFILLED":
+      return {
+        ...state,
+        allItems : _.unionBy([action.payload.data], state.allItems, 'id')
+      }
+    case "DELETE_BUCKETLIST_ITEMS_FULFILLED":
+        const itemId = action.payload.data.item
+        const items = state.allItems
+        const filtereditems = items.filter(item => item.id !== itemId)
+        return {
+          ...state,
+          allItems: filtereditems,
+        }
+    default:
+     return state;
+
+  }
+}
