791cc3a4740a6aedb5a96c470658fcd85b22ad51
justmesam <samuel.wanjiru@andela.com>
2017-09-21 10:56:06 +0300

    [FEATURE] Add the bucketlists reducers which helps on managing the store on the state of the bucketlist after actions are fulfilled

---
+++ src/reducers/bucketlistReducer.js
@@ -0,0 +1,45 @@
+import _ from 'lodash';
+
+const initial_state = {
+  bucketlists: [],
+  singleBucketlist : null,
+};
+
+export default (state=initial_state, action) => {
+  switch (action.type) {
+    case "GET_BUCKETLISTS_FULFILLED":
+      return{
+        ...state,
+        singleBucketlist:null,
+        bucketlists: action.payload.data,
+      }
+    case "GET_ONE_BUCKETLIST_FULFILLED":
+      return {
+        ...state,
+        singleBucketlist: action.payload.data
+      }
+    case "POST_BUCKETLIST_FULFILLED":
+      return {
+        ...state,
+        singleBucketlist:null,
+        bucketlists: _.concat(state.bucketlists, action.payload.data)
+      }
+    case "EDIT_BUCKETLIST_FULFILLED":
+      return {
+        ...state,
+        singleBucketlist:null,
+        bucketlists : _.unionBy([action.payload.data], state.bucketlists, 'id')
+      }
+    case "DELETE_BUCKETLIST_FULFILLED":
+    const bucketlistid = action.payload.data.bucketlist
+    const allBuckets = state.bucketlists
+    const filteredBuckets = allBuckets.filter(bucketlist => bucketlist.id !== bucketlistid)
+        return {
+          ...state,
+          bucketlists: filteredBuckets,
+          singleBucketlist:null
+        }
+    default:
+      return state;
+  }
+}
