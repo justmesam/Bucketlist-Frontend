ea4dafed861282e3555a7fecaaad3827f953f750
justmesam <samuel.wanjiru@andela.com>
2017-09-21 11:43:04 +0300

    [FEATURE] Add the forms for both login and register

---
+++ src/components/logInForm.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import TextField from 'material-ui/TextField'
+import { Field, reduxForm, formValueSelector } from 'redux-form';
+
+const validate = values => {
+  const errors = {}
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+  if (!values.password) {
+    errors.password = 'Required'
+  }
+  return errors
+}
+
+
+const renderTextField = ({
+  input,
+  label,
+  meta: { touched, error },
+  ...custom
+}) =>
+  <TextField
+    hintText={label}
+    floatingLabelText={label}
+    errorText={touched && error}
+    {...input}
+    {...custom}
+  />
+
+
+let LoginForm = props => {
+  const { handleSubmit, pristine, reset, submitting} = props
+  return (
+    <form onSubmit={handleSubmit}>
+      <Field
+        name="email"
+        type="text"
+        component={renderTextField}
+        label="Email"
+      />
+      <Field name="password" type="password" component={renderTextField} label="Password" />
+      <div>
+        <button type="submit" disabled={submitting}>
+          Submit
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
+      </div>
+    </form>
+  )
+}
+
+LoginForm = reduxForm({
+  form: 'loginForm',
+  validate,
+})(LoginForm);
+
+const selector = formValueSelector('loginForm');
+
+
+const mapStateToProps = state => {
+  const email = selector(state, 'email')
+  const password = selector(state, 'password')
+   return {
+      email,
+      password
+   }
+}
+
+
+export default connect(mapStateToProps)(LoginForm)
+
+export { LoginForm }
---
+++ src/components/registerForm.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import TextField from 'material-ui/TextField'
+import { Field, reduxForm, formValueSelector } from 'redux-form'
+
+const validate = values => {
+  const errors = {}
+  if (!values.email) {
+    errors.email = 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+  if (!values.password) {
+    errors.password = 'Required'
+  }
+  if (!values.confirmpassword) {
+    errors.confirmpassword = 'Required'
+  }
+  if (values.password !== values.confirmpassword) {
+    errors.password = 'The passwords must be same'
+    errors.confirmpassword = 'The passwords must be same'
+  }
+  return errors
+}
+
+const renderTextField = ({
+  input,
+  label,
+  meta: { touched, error },
+  ...custom
+}) =>
+  <TextField
+    hintText={label}
+    floatingLabelText={label}
+    errorText={touched && error}
+    {...input}
+    {...custom}
+  />
+
+let RegisterForm = props => {
+  const { handleSubmit, pristine, reset, submitting} = props
+  return (
+    <form onSubmit={handleSubmit}>
+      <Field
+        name="email"
+        type="text"
+        component={renderTextField}
+        label="Email"
+      /><br/>
+      <Field name="password" type="password" component={renderTextField} label="Password" /><br/>
+      <Field name="confirmpassword" type="password" component={renderTextField} label="Confirm password" />
+      <div>
+        <button type="submit" disabled={submitting}>
+          Submit
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
+      </div>
+    </form>
+  )
+}
+
+RegisterForm = reduxForm({
+  form: 'registerForm',
+  validate
+})(RegisterForm);
+
+const selector = formValueSelector('registerForm');
+
+
+const mapStateToProps = state => {
+  const email = selector(state, 'email')
+  const password = selector(state, 'password')
+   return {
+      email,
+      password
+   }
+
+}
+
+export default connect(mapStateToProps)(RegisterForm)
+
+export { RegisterForm }
