58300aa3c2ee83019b00fd1e6512d3ac79d9fe72
justmesam <samuel.wanjiru@andela.com>
2017-09-21 11:07:37 +0300

    [FEATURE] Add the index reducer file which handles combining all the reducers to have one rootreducer

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+import AuthReducer from './authReducer';
+import ItemReducer from './itemReducer';
+import BucketlistReducer from './bucketlistReducer';
+import { reducer as formReducer } from 'redux-form';
+
+const rootReducer = combineReducers({
+  authentication: AuthReducer,
+  form: formReducer,
+  bucketlists: BucketlistReducer,
+  items: ItemReducer,
+});
+
+export default rootReducer;
