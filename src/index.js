75b4a36637968f4836ab636127d815e71b5d07d1
justmesam <samuel.wanjiru@andela.com>
2017-09-21 11:17:39 +0300

    [FEATURE] The root index file which has the react dom and it handles the dom for sending data to the html file and rendering the whole application

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import App from './containers/App';
+import store from './constants/store';
+
+ReactDOM.render(
+  <MuiThemeProvider>
+    <Provider store= { store } >
+      <App />
+    </Provider>
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
