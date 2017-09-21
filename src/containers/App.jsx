b45d45074c10414ac1ea40c29d54719e31132191
justmesam <samuel.wanjiru@andela.com>
2017-09-21 11:56:49 +0300

    [FEATURE] Handles what components will be rendered to the user if they are authenticated

---
+++ src/containers/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Authentication from './Auth';
+import Dashboard from './dashboard';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { authenticated: false };
+  }
+
+  renderView(){
+    const { authenticated, token } = this.props;
+    if(authenticated && token) {
+      return (<Dashboard />)
+    }
+    return  (<Authentication />)
+  }
+  render(){
+    return (
+    <div>
+     <div>
+       {this.renderView()}
+     </div>
+    </div>
+);
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { authenticated, token } = state.authentication;
+  return {
+    authenticated, token
+  }
+}
+export default connect(mapStateToProps)(App);
+export { App }
