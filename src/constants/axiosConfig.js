2d97987bc16aeb3bfe4a7e874c19b02c4ae88403
justmesam <samuel.wanjiru@andela.com>
2017-09-21 11:15:21 +0300

    [FEATURE] Create the axios instsnce for use around the application and also to make it easy for request and response intercepting

---
+++ src/constants/axiosConfig.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import store from './store';
+
+const ApiUrl = 'http://127.0.0.1:5000'
+const instance = axios.create({
+  baseURL : `${ApiUrl}`,
+  timeout : 5000,
+  headers : {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+});
+instance.interceptors.request.use((config) => {
+  const token = store.getState().authentication.token;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token }`;
+  }
+  return config
+});
+
+instance.interceptors.response.use((response) => {
+    return response;
+  }, (error) => {
+    if(error){
+    return Promise.reject(error);
+  }});
+
+export default instance;
