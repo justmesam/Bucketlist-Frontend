86d3315bb2de15f63697d6180ebe92d9e9fd6843
justmesam <samuel.wanjiru@andela.com>
2017-09-23 06:21:46 +0300

    [FEATURE] Add the the change password feature to allow the users to change password at will

--- src/components/layout/navBars.jsx
+++ src/components/layout/navBars.jsx
@@ -1,23 +1,58 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import AppBar from 'material-ui/AppBar';
+import Dialog from 'material-ui/Dialog';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
 import Avatar from 'material-ui/Avatar';
-import { logoutUser } from '../../actions/authActions';
+import { logoutUser,
+        changePassword,
+        deleteUser
+ } from '../../actions/authActions';
+ import ChangePasswordForm from '../changePassword';
 
 
 class NavBar extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      openChange : false
+    }
+  }
+  handleChangeModal = () => {
+    this.setState({openChange : !this.state.openChange})
   }
   handleLogout = () => {
     this.props.logoutUser()
     localStorage.clear()
   }
 
+  handleChangePassword = (values) => {
+    this.setState({
+      old_password : values.old_password,
+      new_password : values.new_password,
+      confirm_password : values.confirm_password
+    })
+    setTimeout(() => {
+      const { old_password,
+              new_password,
+              confirm_password } = this.state
+      this.props.changePassword({
+              old_password,
+              new_password,
+              confirm_password }, 1000)
+    })
+  }
+
   render() {
+    const actionChange = [
+    <FlatButton
+      label="Cancel"
+      primary={true}
+      onClick={this.handleChangeModal}
+    />]
     return (
       <div>
         <AppBar
@@ -34,13 +69,25 @@ class NavBar extends Component {
               targetOrigin={{horizontal: 'right', vertical: 'top'}}
               anchorOrigin={{horizontal: 'right', vertical: 'top'}}
             >
-              <MenuItem primaryText="Change password" />
-              <MenuItem primaryText="Log out" onClick={this.handleLogout}/>
+              <MenuItem primaryText="Change password" onClick={this.handleChangeModal} />
+              <MenuItem primaryText="Log out" onClick={this.handleLogout} />
               <MenuItem primaryText="Delete Account" />
             </IconMenu>
 
           }
         />
+        <Dialog
+         title="Edit a Bucketlist"
+         actions={actionChange}
+         modal={false}
+         open={this.state.openChange}
+         onRequestClose={this.handleChangeModal}
+         autoScrollBodyContent={true}
+       >
+         <ChangePasswordForm
+           onSubmit={this.handleChangePassword}
+         />
+       </Dialog>
       </div>
     );
   }
@@ -51,6 +98,6 @@ const mapStateToProps = (state) => {
   return { token, authenticated }
 }
 
-export default connect(mapStateToProps, {logoutUser})(NavBar)
+export default connect(mapStateToProps, {logoutUser, changePassword})(NavBar)
 
 export { NavBar };
